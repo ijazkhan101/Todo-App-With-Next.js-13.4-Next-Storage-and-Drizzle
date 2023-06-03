9a05d7c131a77ffda79db4ebc5f92a040a3d65ca
ijazdev101 <ijazkhan19932@gmail.com>
2023-06-03 23:19:42 +0500

    home page
    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,8 @@ import AddTodo from "../../src/app/components/AddTodo";
 
 export default function Home() {
   return (
-    <main
-      className="bg-gradient-to-tr from-primary to-secondary h-screen
-   flex justify-center items-center"
-    >
-      <div
-        className="px-6 py-8 rounded-xl bg-gradient-to-br
-       from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md"
-      >
-        <h1>Todo App With Next.js 13.4, Vercel Storage and Drizzle</h1>
+    <main className='flex items-center justify-center h-screen bg-gradient-to-tr from-primary to-secondary'>
+      <div className='px-6 py-8 rounded-xl bg-gradient-to-br from-[#D9D9D9]/50 to-[#D9D9D9]/60  backdrop-blur-xl w-full max-w-md'>
         {/* Todo List */}
         {/* @ts-ignore */}
         <TodoList />
@@ -20,8 +13,8 @@ export default function Home() {
         <AddTodo />
 
         {/* Bar */}
-        <div className="w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-6"></div>
+        <div className='w-1/2 h-1.5 bg-black/80 rounded mx-auto mt-6'></div>
       </div>
     </main>
-  );
+  )
 }
